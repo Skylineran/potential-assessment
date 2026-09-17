d49d6615007ea147c44ca8f2d6b5c59632305077
Skylineran <rygled0808@gmail.com>
2026-09-17 12:12:18 +0800

    add vercel config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path' // 核心：引入 path 模块
+import path from 'path'
 
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
-      // 核心：告诉 Vite，凡是遇到 "@" 开头的路径，都指向 "src" 目录
       '@': path.resolve(__dirname, './src')
     }
   }
